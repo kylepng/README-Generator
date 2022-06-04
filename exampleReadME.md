925b420fad646b1735f0633242b1d4e8bd3a30d9
kyle kumar <kylekumarbusiness@gmail.com>
2022-06-04 09:57:53 -0700

    added new code and inquirer

---
+++ exampleReadME.md
@@ -0,0 +1,2 @@
+<img src='https://img.shields.io/badge/License-Apache_2.0-blue.svg'>
+<h1> Title </h1>
--- index.js
+++ index.js
@@ -1,13 +1,59 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questionnaire = [
+    {
+        message: 'type the name of your project', 
+        type: 'input',
+        name: 'name of the project:'
+    },
+    {
+        message: 'provide an accurate description of your project ', 
+        type: 'input',
+        name: 'description of the project: '
+    },
+    {
+        message: 'what are the installation procedures for this project?', 
+        type: 'input',
+        name: 'installation process: '
+    },
+    {
+        message: 'what licenses does your project have?', 
+        type: 'input',
+        name: 'title'
+    },
+    {
+        message: 'what usages does your project provide?', 
+        type: 'input',
+        name: 'usability'
+    },
+    {
+        message: 'what licenses does your project have?', 
+        type: 'list',
+        choices: ['MIT', 'APACHE 2.0' , 'GPL 3.0', 'BSD 3', 'NONE' ],
+        name: 'license(s):'
+    },
+    {
+        message: 'what are the testing/contributing guidelines that your project has?', 
+        type: 'input',
+        name: 'contributing guidelines:'
+    },
+    {
+        message: 'what is your github username? ', 
+        type: 'input',
+        name: 'github username:',  
+    },
+    {
+        message: 'what is your email?', 
+        type: 'input',
+        name: 'Email:',  
+    },
+]
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+inquirer.prompt(questionnaire)
+.then(response => {
+    console.log(response)
+})
+.catch(err => console.log (err))
